68e89fbfc3e0f8143b9e28e393eb666d8b62387f
cj <cj>
2015-08-05 23:10:52 +0800

    整合echarts

--- WebRoot/indexdata.js
+++ WebRoot/indexdata.js
@@ -1,12 +1,12 @@
-﻿var indexdata = 
+var indexdata = 
 [
     { text: '投诉分析',isexpand:false, children: [ 
-		{url:"login.htm",text:"10086投诉分析"},
+		{url:"echarts.html",text:"10086投诉分析"},
 		{url:"http://localhost/hello",text:"7210086投诉分析"}
 	]
     },
     { text: '物料使用分析', isexpand: false, children: [
-		{ url: "demos/filter/filter.htm", text: "网线使用分析" }
+		{ url: "login.htm", text: "网线使用分析" }
 	]
     }
 ]    
